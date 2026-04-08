38936928a556e182807f8c97eb4e679732e90424
Yeu <yeumensilva9@gmail.com>
2026-04-08 20:58:18 -0300

    Select report location

--- components/homeComponents/ReportModal.tsx
+++ components/homeComponents/ReportModal.tsx
@@ -1,10 +1,18 @@
-import { Dispatch, SetStateAction } from "react";
-import { Modal } from "react-native";
+import * as Location from "expo-location";
+import { Dispatch, SetStateAction, useState } from "react";
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Modal,
+  Platform,
+  ScrollView,
+} from "react-native";
 import styled from "styled-components/native";
-import { Text, View } from "../Themed";
+import { Text, TextInput, View } from "../Themed";
+import DropdownComponent from "./DropDownComponent";
 import ImageSelector from "./ImageSelector";
-
-import { useState } from "react";
+import LocationSelector from "./LocationSelector";
+import MapPickerModal from "./MapPickerModal";
 
 interface ReportModalProps {
   isModalVisible: boolean;
@@ -16,26 +24,95 @@ const ReportModal = ({
   setIsModalVisible,
 }: ReportModalProps) => {
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [selectedCategory, setselectedCategory] = useState<string | null>(null);
+  const [isMapVisible, setIsMapVisible] = useState(false);
+  const [selectedLocation, setSelectedLocation] = useState<string | null>(null);
+  const [selectedCoords, setSelectedCoords] = useState<{
+    lat: number;
+    lng: number;
+  } | null>(null);
+
+  const handleConfirmLocation = async (coords: {
+    latitude: number;
+    longitude: number;
+  }) => {
+    setIsMapVisible(false);
+
+    const reverse = await Location.reverseGeocodeAsync(coords);
+    if (reverse.length > 0) {
+      const addr = reverse[0];
+      setSelectedLocation(`${addr.street} ${addr.name}, ${addr.subregion}`);
+    }
+  };
   return (
     <Modal
       animationType="slide"
       transparent={true}
       visible={isModalVisible}
-      onRequestClose={() => {
-        setIsModalVisible(false);
-      }}
+      onRequestClose={() => setIsModalVisible(false)}
     >
-      <ModalOverlay onPress={() => setIsModalVisible(false)}>
-        <ModalContent>
-          <Indicator />
-          <Text style={{ fontSize: 18, fontWeight: "bold", marginBottom: 20 }}>
-            Reportar Incidente
-          </Text>
-          <ImageSelector image={selectedImage} setImage={setSelectedImage} />
-          <CloseButton onPress={() => setIsModalVisible(false)}>
-            <Text style={{ color: "white" }}>Cerrar</Text>
-          </CloseButton>
-        </ModalContent>
+      <ModalOverlay
+        onPress={() => {
+          Keyboard.dismiss();
+          setIsModalVisible(false);
+        }}
+      >
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={{ width: "100%" }}
+        >
+          <ScrollView
+            bounces={false}
+            keyboardShouldPersistTaps="handled"
+            contentContainerStyle={{ flexGrow: 1, justifyContent: "flex-end" }}
+          >
+            <ModalContent
+              onStartShouldSetResponder={() => {
+                Keyboard.dismiss();
+                return true;
+              }}
+            >
+              <Indicator />
+              <Title>Reportar Incidente</Title>
+
+              <ImageSelector
+                image={selectedImage}
+                setImage={setSelectedImage}
+              />
+
+              <LocationSelector
+                locationName={selectedLocation}
+                setLocationName={setSelectedLocation}
+                setCoords={setSelectedCoords}
+                onOpenMapPicker={() => setIsMapVisible(true)}
+              />
+
+              <MapPickerModal
+                visible={isMapVisible}
+                onClose={() => setIsMapVisible(false)}
+                onConfirm={handleConfirmLocation}
+              />
+              <DropdownComponent
+                selectedCategory={selectedCategory}
+                setselectedCategory={setselectedCategory}
+              />
+
+              <DescriptionText
+                placeholder="Describa el problema aquí..."
+                multiline={true}
+              />
+
+              <ButtonContainer>
+                <CloseButton onPress={() => setIsModalVisible(false)}>
+                  <Text style={{ color: "white" }}>Cerrar</Text>
+                </CloseButton>
+                <SentButton onPress={() => console.log("Enviado")}>
+                  <Text style={{ color: "white" }}>Enviar</Text>
+                </SentButton>
+              </ButtonContainer>
+            </ModalContent>
+          </ScrollView>
+        </KeyboardAvoidingView>
       </ModalOverlay>
     </Modal>
   );
@@ -44,7 +121,6 @@ export default ReportModal;
 
 const ModalOverlay = styled.Pressable`
   flex: 1;
-  background-color: rgba(0, 0, 0, 0.5);
   justify-content: flex-end;
 `;
 
@@ -66,9 +142,40 @@ const Indicator = styled(View)`
   margin-bottom: 15px;
 `;
 
-const CloseButton = styled.Pressable`
+const Title = styled(Text)`
+  font-size: 18;
+  font-weight: bold;
+  margin-bottom: 20;
+`;
+
+const ButtonContainer = styled(View)`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+
+const DescriptionText = styled(TextInput)`
+  width: 100%;
+  height: 160px;
+  border-style: solid;
+  border-color: #4e7ed0;
+  background-color: ${(props) => props.theme.background};
+  border-width: 2px;
+  padding-left: 2%;
+  border-radius: 10px;
+`;
+
+const CloseButton = styled.TouchableOpacity`
   margin-top: 20px;
   background-color: #ff4444;
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+
+const SentButton = styled.TouchableOpacity`
+  margin-top: 20px;
+  background-color: #0e9aec;
   padding: 10px 20px;
   border-radius: 10px;
 `;
--- constants/styled.d.ts
+++ constants/styled.d.ts
@@ -1,10 +1,7 @@
 import "styled-components/native";
-import Colors from "./Colors"; // Esto asume que Colors.ts está en la misma carpeta
-
-// Aquí le decimos: "Tomá la forma de mi objeto Colors.light"
+import Colors from "./Colors";
 type ThemeInterface = typeof Colors.light;
 
 declare module "styled-components/native" {
-  // Aquí "extendemos" la interfaz original para que incluya tus colores
   export interface DefaultTheme extends ThemeInterface {}
 }
